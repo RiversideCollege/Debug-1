497d5520571113eb12800594f2bda827bc567d76
david.petticrew <david.petticrew@CRO-55-15-07>
2017-09-07 12:18:50 +0100

    One more bug

--- Debug 1/Form1.cs
+++ Debug 1/Form1.cs
@@ -16,5 +16,11 @@ namespace Debug_1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            messagebox.show("Hello World)
+        }
     }
 }
+
